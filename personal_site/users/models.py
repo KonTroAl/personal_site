6c3a7cfeb99567a3b58a01ef5126c906f99ae2b9
KonTroAl <troshenkin.k@yandex.ru>
2023-03-17 13:25:34 +0500

    add comments

--- personal_site/users/models.py
+++ personal_site/users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+"""
+на данном этапе пока реализована простейшая модель пользователя без привязки с основному функционалу сайта
+далее поля буду добавляться и редактироваться
+"""
 
-# Create your models here.
+# Модель пользователя
 class User(AbstractUser):
 
     email = models.EmailField(max_length=256, blank=True, unique=True)
