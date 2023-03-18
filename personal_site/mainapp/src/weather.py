7e7bc00a1147bd59485166652b5746523a7e7002
KonTroAl <troshenkin.k@yandex.ru>
2023-03-18 11:27:02 +0500

    update weather.py

--- personal_site/mainapp/src/weather.py
+++ personal_site/mainapp/src/weather.py
@@ -1,28 +1,32 @@
 import requests
 from api_key import API_KEY
-
+"""
+В дальнейшем наименование города будет приходить с frontend
+"""
 city = 'Surgut'
-city_url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city}'
 
-j_data_city = requests.get(city_url).json()
-id_city = j_data_city[0]['Key']
+def forecast(city):
+    # Текстовый поиск города и его ключа/id
+    city_url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city}'
+    j_data_city = requests.get(city_url).json()
+    id_city = j_data_city[0]['Key']
 
-weather_url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{id_city}?apikey={API_KEY}&metric=True'
+    # Поиск погоды в городе по ключу/id
+    weather_url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{id_city}?apikey={API_KEY}&metric=True'
+    j_data_forecast = requests.get(weather_url).json()
+    weather_headline = j_data_forecast['Headline']
+    weather_daily_forecasts = j_data_forecast['DailyForecasts']
+    temperature = weather_daily_forecasts[0]['Temperature']
 
-j_data_forecast = requests.get(weather_url).json()
-
-weather_headline = j_data_forecast['Headline']
-weather_daily_forecasts = j_data_forecast['DailyForecasts']
-temperature = weather_daily_forecasts[0]['Temperature']
-
-weather = {
-    'Text': weather_headline['Text'],
-    'Temperature': {
-        'Maximum': temperature['Maximum']['Value'],
-        'Minimum': temperature['Minimum']['Value'],
+    # Заполнение словаря необходимыми данными для дальнейшей работы
+    weather = {
+        'Text': weather_headline['Text'],
+        'Temperature': {
+            'Maximum': temperature['Maximum']['Value'],
+            'Minimum': temperature['Minimum']['Value'],
+        }
     }
-}
+    return weather
 
-print(weather)
 
 
